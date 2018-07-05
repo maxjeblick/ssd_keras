b74f73f3c8c6f9c5b1e829a6a93feb21d57d5e65
maxjeblick <maxjeblick@users.noreply.github.com>
2018-07-05 16:37:56 +0200

    Add files via upload
    added setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools import find_packages
+
+long_description = '''
+nnn'''
+
+setup(name='Keras',
+      version='2.2.0',
+      description='nnn',
+      long_description=long_description,
+      author='nnn',
+      author_email='nnn',
+      url='https://github.com/maxjeblick/ssd_keras',
+      download_url='https://github.com/maxjeblick/ssd_keras',
+      license='MIT',
+      install_requires=[],
+      extras_require={
+      },
+      classifiers=[
+      ],
+      packages=find_packages())
